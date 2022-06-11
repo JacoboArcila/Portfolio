ba87c4b1476ed62c9fbf601c1ca7904df52a9fe6
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-06-11 12:15:43 -0500

    adding packages, folders and paths

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        background-color: #023047;
+        color: white;
+    }
+`
+export default GlobalStyles;
