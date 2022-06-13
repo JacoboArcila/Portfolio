b5c888a9420aa24dc0073b8111f51a365957b8bc
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-06-13 12:40:55 -0500

    C:/Program Files/Git/portfolio

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <GlobalStyles />
         <Nav />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/portfolio' element={<Home />} />
           <Route path='/education' element={<Education />} />
           <Route path='/projects' element={<Projects />} />
           <Route path='/contact' element={<Contact />} />
